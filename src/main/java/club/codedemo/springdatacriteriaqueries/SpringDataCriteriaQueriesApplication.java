240e5a2e210323efdf25bd7ace3d637258293a7d
panjie <3792535@qq.com>
2020-08-05 19:06:46 +0800

    init

---
+++ src/main/java/club/codedemo/springdatacriteriaqueries/SpringDataCriteriaQueriesApplication.java
@@ -0,0 +1,13 @@
+package club.codedemo.springdatacriteriaqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataCriteriaQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataCriteriaQueriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/codedemo/springdatacriteriaqueries/SpringDataCriteriaQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package club.codedemo.springdatacriteriaqueries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataCriteriaQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
